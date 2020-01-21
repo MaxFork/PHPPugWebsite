78c574b7dac075879c2f5db83bf22d49585662fd
Max Base <Max@Asrez.com>
2020-01-21 17:09:46 +0330

    Adding Persian language to website.

---
+++ resources/lang/fa/index.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'title'              => 'Phug (unified pug engine for Pug-php and Tale-pug)',
+    'edit'               => 'ویرایش',
+    'last_release'       => 'آخرین نسخه پایدار',
+    'build_status'       => 'ساخت وضعیت',
+    'try_phug'           => 'Phug را امتحان کنید',
+    'language_reference' => 'مرجع زبان',
+    'resources'          => 'منابع',
+    'api_reference'      => 'مرجع API',
+    'toggle_sidebar'     => 'تغییر نوار کناری',
+];
